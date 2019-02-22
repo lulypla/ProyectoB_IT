81058f576c8e71d40c745f73d3632ed2ff1a1a90
unknown <paolapuchetta8Qgmail.com>
2019-02-22 16:27:49 -0300

    Paginas estáticas con sus enlaces y css de cada una

--- app/server.py
+++ app/server.py
@@ -7,10 +7,38 @@ from flask import Flask, request, render_template, url_for, session, json, redir
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def index():
 	# el servidor arranca con esta página raiz
     return app.send_static_file('index.html')
+
+
+@app.route('/ofertas', methods=['GET','POST'])
+def ofertas():
+	# pagina ofertas
+    return app.send_static_file('ofertas.html')
+
+@app.route('/centros', methods=['GET','POST'])
+def centros():
+	# listado de comercios adheridos y centros de canje 
+    return app.send_static_file('centros.html')
+
+@app.route('/EcoBits', methods=['GET','POST'])
+def EcoBits():
+	# sobre EcoBits 
+    return app.send_static_file('EcoBits.html')
+
+@app.route('/contacto', methods=['GET','POST'])
+def contacto():
+	# Contacto - formulario, teléfono,...
+    return app.send_static_file('contacto.html')
+
+@app.route('/FAQ', methods=['GET','POST'])
+def faq():
+	# preguntas frecuentes 
+    return app.send_static_file('faq.html')
+
+
 
 
 # levanta el servidor con el método run()
