6ce3dc51f563d1cedf7e6c3bbec1891771b3a512
LNassutti <lnassutti@inconcertcc.com.uy>
2019-03-19 21:01:07 -0300

    estilos

--- app/static/js/validaciones.js
+++ app/static/js/validaciones.js
@@ -24,3 +24,23 @@ function password(pass1, pass2) {
   // No se si quieres tambien alguna validacion por largo o elemntos de seguridad.
   return iguales(pass1, pass2);
 }
+
+
+const Validate = objForm => {
+  var email = objForm[(input = "email")].value;
+  var repetir_email = objForm[(input = "repetir_email")].value;
+  var password = objForm[(input = "password")].value;
+  var repetir_password = objForm[(input = "repetir_password")].value;
+
+  if (email === repetir_email) {
+    if (password === repetir_password) {
+      return true;
+    } else {
+      alert("contraseñas no coinciden");
+      return false;
+    }
+  } else {
+    alert("email no coinciden");
+    return false;
+  }
+};
