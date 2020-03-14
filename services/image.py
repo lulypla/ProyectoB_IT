acf98f140f7d397880fe9f53dd74747158121071
Debray Peralta <debray.peralta@gmail.com>
2020-03-14 17:11:49 -0300

    Changed config to not be GC

--- services/image.py
+++ services/image.py
@@ -1,13 +1,14 @@
 import cloudinary
 import cloudinary.uploader
 
-cloudinary.config(
-  cloud_name = "da29ifelj",
-  api_key = "949499613611759",
-  api_secret = "v05os3Owzc3SrB8T-KSUMBBY3e4"
-)
 
 
 def upload_image(image_data):
+    cloudinary.config(
+        cloud_name="da29ifelj",
+        api_key="949499613611759",
+        api_secret="v05os3Owzc3SrB8T-KSUMBBY3e4"
+    )
+
     url = cloudinary.uploader.upload(image_data)
     return url['secure_url']
